2e872cdc36577a3d5808b063b32b78974e72c155
adam.ismaili <adam.ismaili@epita.fr>
2023-03-28 18:50:04 +0200

    main.py modified for stage2

--- stage2/sources/main.py
+++ stage2/sources/main.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, HTTPException
 import numpy as np
 
 import cv2
@@ -20,11 +20,11 @@ def greet_person(who: Union[str, None] = None):
 async def imgshape(file: UploadFile):
     width = height = depth = 0
     bytes_as_np_array = np.frombuffer(await file.read(), dtype=np.uint8)
-    try:
-        img = cv2.imdecode(bytes_as_np_array, cv2.IMREAD_COLOR)
-        if img is not None:
-            height, width, depth = img.shape
-    except Exception as e:
-        return {"error": str(e)}
+    img = cv2.imdecode(bytes_as_np_array, cv2.IMREAD_COLOR)
+    
+    if img is not None:
+        height, width, depth = img.shape
+    else:
+        raise HTTPException(status_code=500, detail="Cannot find image data in file '" + file.filename + "'.")
 
     return {"width": width, "height": height, "depth": depth}
