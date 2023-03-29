51dcdd4af90415951725b03d8bf2c626bb7cd4ed
Scott TALLEC <scott.tallec@gmail.com>
2023-03-29 16:34:56 +0200

    fix - stage3 done

--- stage3/sources/test_ocr.py
+++ stage3/sources/test_ocr.py
@@ -15,27 +15,28 @@ config.read(config_path)
 # inside the config file.
 page_parser = PageParser(config, config_path=os.path.dirname(config_path))
 
-
+# get home directory
+home_directory = os.path.expanduser( '~' )
 #iterate for every *.jpg
-directory = "/data/input"
+directory = home_directory + "/data/input"
 
-for image in os.listdir(directory):
-    filepath = os.path.join(directory, image)
+for filename in os.listdir(directory):
+    filepath = os.path.join(directory, filename)
     if os.path.isfile(filepath) and (filename.endswith(".jpg") or filename.endswith(".jpeg")):
         # Read the document page image.
         image = cv2.imread(filepath, 1)
 
         # Init empty page content. 
         # This object will be updated by the ocr pipeline. id can be any string and it is used to identify the page.
-        page_layout = PageLayout(id=input_image_path,
+        page_layout = PageLayout(id=filepath,
              page_size=(image.shape[0], image.shape[1]))
 
         # Process the image by the OCR pipeline
         page_layout = page_parser.process_page(image, page_layout)
 
-        # Save output
-        page_layout.to_altoxml('/data/output/jo-sample1.ALTO.xml') # Save results as ALTO XML.
 
+        # Save output
+        page_layout.to_altoxml(os.path.join(home_directory + "/data/output/", os.path.splitext(filename)[0]+ ".ALTO.xml")) # Save results as ALTO XML.
         # Bonus: display the transcription in the console
         print("Transcription:")
         print("="*50)
